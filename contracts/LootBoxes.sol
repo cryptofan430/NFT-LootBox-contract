e0417c0e7f7837c2ed2aae70085f1435d833d5f0
cryptoPanda430 <panda.kk1114@gmail.com>
2022-05-14 14:16:19 +0100

    Update for rng

--- contracts/LootBoxes.sol
+++ contracts/LootBoxes.sol
@@ -1,35 +1,116 @@
 // SPDX-License-Identifier: GPL-3.0
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-contract LootBoxes {
+contract LootBoxes is Ownable {
     string[3] public items          = ["Dragon Sword", "Silver Sword", "Common Sword"];
-    uint  [3] public probabilities  = [10,  20, 70];
-    // address   public gameDeveloper  = 0x00A877E0325Ba6c697f5a6ae05Fa8F91b544276C;
+    uint  [3] public probabilities  = [7000, 2100, 400, 400, 50, 50];
        
-    mapping (string => uint) private blockLastPurchase;
+    // mapping (string => uint) private blockLastPurchase;
+    address private _treasury;
+    IERC20 TOKEN;
+    IERC721 NFT;
+    uint256 seed;
+    uint256 constant INVERSE_BASIS_POINT = 10000;
+    uint public price = 1;
+    // Must be sorted by rarity
+    enum Class {
+        Common,
+        Rare,
+        Epic,
+        Legendary,
+        Divine,
+        Hidden
+    }
+    struct Loot {
+        uint gsm;
+        uint[] nft;
+    }
+
+    uint256 constant NUM_CLASSES = 6;
+    function setProbabilities(uint[] memory _value) external {
+        probabilities = _value;
+    }
+    mapping(address => Loot) public earned;
+    struct OptionSettings {
+        // Number of items to send per open.
+        // Set to 0 to disable this Option.
+        uint256 maxQuantityPerOpen;
+        // Probability in basis points (out of 10,000) of receiving each class (descending)
+        uint16[NUM_CLASSES] classProbabilities;
+        // Whether to enable `guarantees` below
+        bool hasGuaranteedClasses;
+        // Number of items you're guaranteed to get, for each class
+        uint16[NUM_CLASSES] guarantees;
+    }
 
     event Draws(string item, string screenName);
-        
-    function drawItem(string memory screenName) public returns(uint){
 
-        assert(block.number != blockLastPurchase[screenName]); 
-        // assert(msg.sender == gameDeveloper);
-        
-        blockLastPurchase[screenName] = block.number;
-                
-        uint randomValue   = RNG(screenName);
-        uint lowerBoundary = 0;
-        for (uint i=0; i< items.length; i++) {
-            if (randomValue >= lowerBoundary && randomValue < lowerBoundary + probabilities[i]) {
-                // emit Draws(items[i], screenName);
-                return i;
-            }
-            else {
-                lowerBoundary = lowerBoundary + probabilities[i];
-            }
-        }
+    event TreasuryUpdated(address previous, address updated);
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _initBaseURI,
+        address _GSM
+    ) ERC721(_name, _symbol) {
+        GSM = IERC20(_GSM);
+        setBaseURI(_initBaseURI);
+        mint(0xcf9b1f007f246c1D86735941Aeb4eddBc8C0016F, 104);
     }
     
-    function RNG(string memory screenName) private view returns (uint8) { 
-        return uint8(uint256(keccak256(abi.encodePacked(block.timestamp, screenName)))%100);
+
+    function addItem(string memory itemName, uint8 probability ) public onlyOwner(){
+
+    }
+
+    function claim() external {
+        TOKEN.transfer(msg.sender, earned[msg.sender].gsm);
+        for(uint i = 0; i < earned[msg.sender].nft.length; i ++) {
+            // NFT.tran
+        }
+    }
+
+    function pickRandomItem() public view returns(string memory){
+        require(TOKEN.balanceOf(msg.sender) >= price, "insufficient balance");
+        TOKEN.transferFrom(msg.sender, address(this), price);
+
+        uint16 value = uint16(_random().mod(INVERSE_BASIS_POINT));
+
+        for (uint256 i = probabilities.length - 1; i > 0; i--) {
+            uint16 probability = probabilities[i];
+            if (value < probability) {
+                return Class(i);
+            } else {
+                value = value - probability;
+            }
+        }
+        Loot tmp = Loot(gsm, nft);
+        earned[msg.sender] = Loot;
+        //
+        return Class.Common;
+    }
+    
+     /**
+    * @dev Improve pseudorandom number generator by letting the owner set the seed manually,
+    * making attacks more difficult
+    * @param _newSeed The new seed to use for the next transaction
+    */
+    function setSeed(uint256 _newSeed) external onlyOwner {
+        seed = _newSeed;
+    }
+
+    function setTreasury(address treasury_) external onlyOwner{
+        emit TreasuryUpdated(_treasury, treasury_);
+        _treasury = treasury_;
+    }
+
+    function withdraw() external onlyOwner {
+      TOKEN.transfer(address(this).balance, treasury);
+    }
+
+    function _random() internal returns (uint256) {
+        uint256 randomNumber = uint256(keccak256(abi.encodePacked(blockhash(block.number - 1), msg.sender, seed)));
+        seed = randomNumber;
+        return randomNumber;
     }
 }
--- frontend/src/components/Greeter.tsx
+++ frontend/src/components/Greeter.tsx
@@ -1,17 +1,20 @@
 import React, { useContext, useEffect, useState } from "react";
-import { GreeterContext } from "./../hardhat/SymfoniContext";
+import { LootBoxesContext } from "./../hardhat/SymfoniContext";
+import { ethers } from "ethers";
+import LootBoxesDeployment from "./../hardhat/deployments/ropsten/LootBoxes.json";
+
 
 interface Props {}
 
 export const Greeter: React.FC<Props> = () => {
-  const greeter = useContext(GreeterContext);
+  const greeter = useContext(LootBoxesContext);
   const [message, setMessage] = useState("");
   const [inputGreeting, setInputGreeting] = useState("");
   useEffect(() => {
     const doAsync = async () => {
       if (!greeter.instance) return;
       console.log("Greeter is deployed at ", greeter.instance.address);
-      setMessage(await greeter.instance.greet());
+      // setMessage(await greeter.instance.greet());
     };
     doAsync();
   }, [greeter]);
@@ -20,14 +23,42 @@ export const Greeter: React.FC<Props> = () => {
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.preventDefault();
+    
+    
     if (!greeter.instance) throw Error("Greeter instance not ready");
     if (greeter.instance) {
-      const tx = await greeter.instance.setGreeting(inputGreeting);
-      console.log("setGreeting tx", tx);
-      await tx.wait();
-      const _message = await greeter.instance.greet();
-      console.log("New greeting mined, result: ", _message);
-      setMessage(_message);
+      // const tx = await greeter.instance.setGreeting(inputGreeting);
+      // console.log("setGreeting tx", tx);
+      // await tx.wait();
+
+      // const provider = new ethers.providers.Web3Provider(window.ethereum)
+      // const signer = provider.getSigner();
+
+      // const contract = new ethers.Contract(greeter.instance?.address, LootBoxesDeployment.abi, signer)
+      
+      // try{
+      //   let result = await contract.drawItem(inputGreeting)
+      //   console.log(result, "------")
+      // } catch(err) {
+      //   console.log("Error: ", err)
+      // }
+
+
+      const tx = await greeter.instance.drawItem(inputGreeting);
+      
+      console.log("New greeting mined, result: ", tx);
+      
+      setMessage(tx);
+
+      // greeter.instance.on("Draws", (from, to, value, event) => {
+
+      //   console.log(from,to,value);
+    
+      //   console.log(event.blockNumber);
+    
+      // });
+
+      
       setInputGreeting("");
     }
   };
